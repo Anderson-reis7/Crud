734adbe965d65cddb844d1e332c97424a94e2b29
Anderson Reis <Anderson.200810@gmail.com>
2023-08-29 13:00:07 -0300

    Add columa ative

--- src/main/java/com/exemplo/crud/domain/product/Product.java
+++ src/main/java/com/exemplo/crud/domain/product/Product.java
@@ -18,11 +18,13 @@ public class Product {
     private Integer id;
     private String name;
     private Integer price_in_cents;
+    private Boolean active;
 
     public Product(ResquestProductDTO resquestProductDTO){
         this.id = resquestProductDTO.id();
         this.name = resquestProductDTO.name();
         this.price_in_cents = resquestProductDTO.price_in_cents();
+        this.active = true;
     }
 }
 
