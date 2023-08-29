52a297a8235185fc8edab5767b11dd76cd0e2224
Anderson Reis <Anderson.200810@gmail.com>
2023-08-29 13:42:21 -0300

    Criação de classe para tratar error

---
+++ src/main/java/com/exemplo/crud/infra/RequestsException.java
@@ -0,0 +1,15 @@
+package com.exemplo.crud.infra;
+
+import jakarta.persistence.EntityNotFoundException;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.ExceptionHandler;
+import org.springframework.web.bind.annotation.RestControllerAdvice;
+
+@RestControllerAdvice
+public class RequestsException {
+    @ExceptionHandler(EntityNotFoundException.class)
+    public ResponseEntity threat404(){
+        return ResponseEntity.badRequest().body("Dado não encontrado!");
+    }
+
+}
